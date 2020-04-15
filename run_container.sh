0bf27dbb76b04f43c313a3ea7a5fc5b2e5dc6390
soujiro0725 <soujiro0725@gmail.com>
2020-04-15 19:50:22 +0900

    notebooks added

--- run_container.sh
+++ run_container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker pull tensorflow/tensorflow:latest-py3
-docker run -it -p 8888:8888 tensorflow/tensorflow:latest-py3-jupyter
+docker pull lspvic/tensorboard-notebook
+#docker run -it -p 8888:8888 tensorflow/tensorflow:latest-py3-jupyter
+docker run -it --rm -p 8888:8888 -v "$(pwd)/notebooks:/home/jovyan/work" lspvic/tensorboard-notebook
 
